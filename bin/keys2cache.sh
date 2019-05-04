a72f3c0a203814293deeafb4f98227f0eaf300da
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-05-04 16:58:40 -0400

    Adding texts

---
+++ bin/keys2cache.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# keys2cache.sh - query a database for keys and URL, and pipe the result to a cacheing application
+
+# Eric Lease Morgan <eric_morgan@infomotions.com>
+# May 4, 2019 - first cut; cool!
+
+
+# configure
+DB='./etc/gutenberg.db'
+SQL="select t.gid, f.file from titles as t, files as f where t.gid = f.gid and file like '%.txt.utf-8' AND t.language IS 'en' order by t.gid limit 1000;"
+URL2CACHE='./bin/url2cache.sh'
+
+# execute the SQL, parse, do the work, and done
+echo $SQL | sqlite3 $DB | tr "|" " " | parallel --colsep ' ' $URL2CACHE {1} {2}  
+exit
---
+++ bin/url2cache.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# url2cache.sh - given a key and a URL, cache a file 
+
+# Eric Lease Morgan <eric_morgan@infomotions.com>
+# May 4, 2019 - first cut
+
+
+# configure
+DIRECTORY='./texts'
+STRIP='./bin/strip.pl'
+TMP='./tmp'
+
+# get input
+KEY=$1
+URL=$2
+
+# initialize
+OUTPUTFILE="$DIRECTORY/$KEY.txt"
+
+# conditionally configure, harvest, strip, save, and clean up
+if [[ ! -f $OUTPUTFILE ]]; then
+
+	TEMPORARYFILE="$TMP/$KEY.txt"
+	wget -nv -O $TEMPORARYFILE $URL
+	$STRIP $TEMPORARYFILE > $OUTPUTFILE
+	rm $TEMPORARYFILE
+
+fi
+
+# done
+exit
+
